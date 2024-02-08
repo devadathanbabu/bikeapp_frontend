70b43877e36cea173cc2af22467b5e669ee032ce
devadathanbabu <devadathanbabu27@gmail.com>
2024-02-09 01:12:08 +0530

    add completed

--- lib/service/bikeservice.dart
+++ lib/service/bikeservice.dart
@@ -1,6 +1,41 @@
+import 'dart:convert';
+
 import 'package:bikeapp/model/bikemodel.dart';
 import 'package:http/http.dart' as http;
 class BikeApiService{
+  Future<dynamic> sendData(
+      String name,
+      String model,
+      String yearofManu,
+      String cc,) async{
+    var client = http.Client();
+    var apiUrl = Uri.parse("http://192.168.1.4:3001/api/bike/addbike");
+    var response = await client.post(apiUrl,
+        headers: <String,String>{
+          "Content-Type" : "application/json; charset=UTF-8"
+        },
+        body: jsonEncode(<String,String>
+        {
+          "name": name,
+          "model": model,
+          "yearofManu":yearofManu,
+          "cc":cc,
+        }
+        )
+    );
+
+    if(response.statusCode==200)
+    {
+      return json.decode(response.body);
+    }
+    else
+    {
+      throw Exception("Failed to send data");
+    }
+
+  }
+
+
   Future<List<Bike>> getData() async{
     var client=http.Client();
     var ApiUrl=Uri.parse("http://192.168.1.4:3001/api/bike/viewbike");
